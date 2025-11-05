49fbfd5aa00d392ff77cae3d836274cf13833db7
Karina Dudko <133782806+karin4dq@users.noreply.github.com>
2025-11-05 20:56:34 +0100

    Update transportujFlota.hpp

--- transportujFlota.hpp
+++ transportujFlota.hpp
@@ -2,8 +2,27 @@
 
 #include "Stocznia.hpp"
 
+
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
-    return 0;
+    unsigned int sumaTowaru = 0;
+    unsigned int liczbaZaglowcow = 0;
+
+    Stocznia stocznia{};
+
+    while (sumaTowaru < towar)
+    {
+    
+    Statek* s1 = stocznia();
+
+    unsigned int przewiez = s1->transportuj();
+    sumaTowaru += przewiez;
+
+    if (dynamic_cast<Zaglowiec*>(s1))
+        liczbaZaglowcow++;
+
+    delete s1;
+    }
+
+    return  liczbaZaglowcow;
 }
